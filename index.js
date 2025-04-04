636f6807d869d7e4fca858d98be26aba906afc7a
4313657 <4313657@TE-GDA-D-D21E01>
2025-04-04 09:15:04 +0200

    zadanie pozdrawiam mame nerda

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-console.log('Hello, World!');
-
 const express = require('express');
-
 const app = express();
+const port = 3000;
 
-const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    console.log('Żądanie GET otrzymane dla strony głównej',  req.query);
-  res.send('Hello, World!');
-}); 
-
-app.post('/', (req, res) => {
-    console.log('Żądanie POST otrzymane dla strony głównej', req);
-  res.send('Hello, World!');
+app.use((req, res, next) => {
+    console.log('Żądanie: ', req.method, req.url);
+    next();
 });
 
-app.listen(PORT, () => {
-    console.log(`aplikacja pracuja na porcie http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+    console.log('Żądanie GET dla strony głównej ', req.query);
+    res.send('Hello World!');
+});
+app.post('/', (req, res) => {
+    console.log('Żądanie POST dla strony głównej ', req.body);
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Aplikacja pracuje pod adresem http://localhost:${port}`);
 });
